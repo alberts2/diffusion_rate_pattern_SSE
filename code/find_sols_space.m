64e3ca051167cf4674724321376beac471f0295e
Albert Soewongsono <albertsoewongsono@Alberts-MacBook-Pro.local>
2025-08-18 12:22:45 -0500

    added more scenarios for BiSSE

--- code/find_sols_space.m
+++ code/find_sols_space.m
@@ -70,146 +70,445 @@ minQBA = Minimize[{qBA, constraints}, {lambdaA, lambdaB, muA, muB, qAB, qBA}]
 (**)
 
 
-(* ::Input:: *)
-(**)
+(*stationary = [0.6,0.4]*)
+(*Solution surface for lambdaA, qBA and muA*)
 
+(*Fixed values for qAB, muB, and lambdaB*)
 
-(* ::Input:: *)
-(**)
-
-
-(* ::Input:: *)
-(**)
-
-
-(* ::Input:: *)
-(**)
-
-
-(* ::Input:: *)
-(**)
-
-
-(*Solution surface for lambdaA, qAB a,d qBA*)
-(* Fixed values for muA and muB *)
-muAVal = 1;
-muBVal = 1;
-
-(* RegionPlot3D with lambdaB replaced by its formula *)
-rule1 = RegionPlot3D[
-  (* Constraints *)
-  muAVal > 1/3*(-3*qAB + 2*qBA) &&
-  qBA > 3*qAB/2 &&
-  (1/3*(3*muAVal + 3*qAB - 2*qBA) > 0) &&
-  (1/2*(2*muBVal - 3*qAB + 2*qBA)) > 0,
-  
-  (* Plot variables *)
-  {lambdaA, 0, 1}, {qAB, 0, 1}, {qBA, 0, 1},
-(*  {lambdaA, 0, 5}, {qAB, 0, 5}, {qBA, 0, 5},*)
-  
-  PlotPoints -> 50,
-  Mesh -> None,
-  PlotStyle -> Opacity[0.3, Gray]
-];
-
-rule2 = RegionPlot3D[
-  (* Constraints *)
-  qBA < 3*qAB/2 &&
-  muBVal > 1/2*(3*qAB - 2*qBA) &&
-  (1/3*(3*muAVal + 3*qAB - 2*qBA) > 0) &&
-  (1/2*(2*muBVal - 3*qAB + 2*qBA)) > 0,
-  
-  (* Plot variables *)
-  {lambdaA, 0, 1}, {qAB, 0, 1}, {qBA, 0, 1},
- (* {lambdaA, 0, 5}, {qAB, 0, 5}, {qBA, 0, 5},*)
-  
-  PlotPoints -> 50,
-  Mesh -> None,
-  PlotStyle -> Opacity[0.3, Blue]
-];
-
-combinedPlot = Show[rule1, rule2, AxesLabel -> {"\[Lambda]A", "qAB", "qBA"},ViewPoint -> {0, -5, 1}]
-
-
-
-(*Solution surface for lambdaA, qBA a,d muA*)
-(* Fixed values for muA and muB *)
+(*this scenario satisfies ruleset 1 but not ruleset 2*)
+(*one example will gives us under ruleset 1 qBA = 0.2, muA = 0.1, lambdaA =0.2/3*)
 qABVal = 0.1;
 muBVal = 0.1;
+lambdaBVal = 0.2;
 
-(* RegionPlot3D with lambdaB replaced by its formula *)
-rule1 = RegionPlot3D[
-  (* Constraints *)
-  muA > 1/3*(-3*qABVal + 2*qBA) &&
-  qBA > 3*qABVal/2 &&
+(*this scenario satisfies ruleset 2 but not ruleset 1*)
+(*one example will give us under ruleset 2 qBA=0.1, muA = 0.1 and lambdaA = 0.4/3*)
+(*qABVal = 0.1;
+muBVal = 0.2;
+lambdaBVal = 0.1 ;*)
+(*tolerance for the lambdaA != muA (and for B)*)
+tol = 0.00001;
+(*Note LambdaB cannot be equal to MuA. Similarly for LambdaA and MuA.*)
+
+(*Ruleset 1*)
+
+(*plot the region bounded by the inequalities *)
+region1 = RegionPlot3D[
   (1/3*(3*muA + 3*qABVal - 2*qBA) > 0) &&
-  (1/2*(2*muBVal - 3*qABVal + 2*qBA)) > 0,
-  
-  (* Plot variables *)
+  (1/2*(2*muBVal - 3*qABVal + 2*qBA)) > 0 &&
+   muA > 1/3*(-3*qABVal + 2*qBA) &&
+   qBA > 3*qABVal/2 &&
+   Abs[lambdaA - muA] > tol &&
+   Abs[lambdaBVal - muBVal] > tol,
   {lambdaA, 0, 1}, {qBA, 0, 1}, {muA, 0, 1},
-  
-  PlotPoints -> 50,
-  Mesh -> None,
-  PlotStyle -> Opacity[0.3, Gray]
+  PlotStyle -> Opacity[0.2, Gray],
+  Mesh -> None
 ];
 
-rule2 = RegionPlot3D[
-  (* Constraints *)
-  qBA < 3*qABVal/2 &&
-  muBVal > 1/2*(3*qABVal - 2*qBA) &&
+(* plot the plan for lambdaA equality *)
+surf1 = ContourPlot3D[
+  lambdaA - 1/3*(3*muA + 3*qABVal - 2*qBA) == 0,
+  {lambdaA, 0, 1}, {qBA, 0, 1}, {muA, 0, 1},
+  RegionFunction -> Function[{lambdaA, qBA, muA},
+    muA > 1/3*(-3*qABVal + 2*qBA) &&
+    qBA > 3*qABVal/2 &&
+    (1/3*(3*muA + 3*qABVal - 2*qBA) > 0) &&
+    (1/2*(2*muBVal - 3*qABVal + 2*qBA)) > 0 &&
+     Abs[lambdaA - muA] > tol &&
+     Abs[lambdaBVal - muBVal] > tol
+  ],
+  PlotPoints -> 30, MaxRecursion -> 2,
+   Mesh -> None,
+  ContourStyle -> Opacity[1, Red]
+];
+
+(* plot the plan for lambdaB equality *)
+surf2 = ContourPlot3D[
+  lambdaBVal - 1/2*(2*muBVal - 3*qABVal + 2*qBA) == 0,
+   {lambdaA, 0, 1}, {qBA, 0, 1}, {muA, 0, 1},
+  RegionFunction -> Function[{lambdaA, qBA, muA},
+    muA > 1/3*(-3*qABVal + 2*qBA) &&
+    qBA > 3*qABVal/2 &&
+    (1/3*(3*muA + 3*qABVal - 2*qBA) > 0) &&
+    (1/2*(2*muBVal - 3*qABVal + 2*qBA)) > 0 &&
+     Abs[lambdaA - muA] > tol &&
+     Abs[lambdaBVal - muBVal] > tol
+  ],
+  PlotPoints -> 30, MaxRecursion -> 2,
+   Mesh -> None,
+  ContourStyle -> Opacity[1, Yellow]
+];
+
+(*Ruleset 2*)
+
+(*plot the region bounded by the inequalities *)
+region2 = RegionPlot3D[
   (1/3*(3*muA + 3*qABVal - 2*qBA) > 0) &&
-  (1/2*(2*muBVal - 3*qABVal + 2*qBA)) > 0,
-  
-  (* Plot variables *)
+  (1/2*(2*muBVal - 3*qABVal + 2*qBA)) > 0 &&
+   qBA < 3*qABVal/2 &&
+   muBVal > 1/2*(3*qABVal - 2*qBA)&&
+   Abs[lambdaA - muA] > tol,
   {lambdaA, 0, 1}, {qBA, 0, 1}, {muA, 0, 1},
-  
-  PlotPoints -> 50,
-  Mesh -> None,
-  PlotStyle -> Opacity[0.3, Blue]
+  PlotStyle -> Opacity[0.2, Blue],
+  Mesh -> None
 ];
 
-combinedPlot = Show[rule1, rule2, AxesLabel -> {"\[Lambda]A", "qBA", "muA"},ViewPoint -> {0, -5, 1}]
-
-
-
-(*Solution surface for lambdaB, qAB a,d muB*)
-(* Fixed values for muA and muB *)
-qBAVal = 0.1;
-muAVal = 0.1;
-
-(* RegionPlot3D with lambdaB replaced by its formula *)
-rule1 = RegionPlot3D[
-  (* Constraints *)
-  muAVal > 1/3*(-3*qAB + 2*qBAVal) &&
-  qBAVal > 3*qAB/2 &&
-  (1/3*(3*muAVal + 3*qAB - 2*qBAVal) > 0) &&
-  (1/2*(2*muB - 3*qAB + 2*qBAVal)) > 0,
-  
-  (* Plot variables *)
-  {lambdaB, 0, 1}, {qAB, 0, 1}, {muB, 0, 1},
-  
-  PlotPoints -> 50,
-  Mesh -> None,
-  PlotStyle -> Opacity[0.3, Gray]
+(* plot the plan for lambdaA equality *)
+surf3 = ContourPlot3D[
+  lambdaA - 1/3*(3*muA + 3*qABVal - 2*qBA) == 0,
+  {lambdaA, 0, 1}, {qBA, 0, 1}, {muA, 0, 1},
+  RegionFunction -> Function[{lambdaA, qBA, muA},
+   (1/3*(3*muA + 3*qABVal - 2*qBA) > 0) &&
+  (1/2*(2*muBVal - 3*qABVal + 2*qBA)) > 0 &&
+   qBA < 3*qABVal/2 &&
+   muBVal > 1/2*(3*qABVal - 2*qBA)&&
+   Abs[lambdaA - muA] > tol
+  ],
+  PlotPoints -> 30, MaxRecursion -> 2,
+   Mesh -> None,
+  ContourStyle -> Opacity[1, Red]
 ];
 
-rule2 = RegionPlot3D[
-  (* Constraints *)
-  qBAVal < 3*qAB/2 &&
-  muB > 1/2*(3*qAB - 2*qBAVal) &&
-  (1/3*(3*muAVal + 3*qAB - 2*qBAVal) > 0) &&
-  (1/2*(2*muB - 3*qAB + 2*qBAVal)) > 0,
-  
-  (* Plot variables *)
-  {lambdaB, 0, 1}, {qAB, 0, 1}, {muB, 0, 1},
-  
-  PlotPoints -> 50,
-  Mesh -> None,
-  PlotStyle -> Opacity[0.3, Blue]
+(* plot the plan for lambdaB equality *)
+surf4 = ContourPlot3D[
+  lambdaBVal - 1/2*(2*muBVal - 3*qABVal + 2*qBA) == 0,
+   {lambdaA, 0, 1}, {qBA, 0, 1}, {muA, 0, 1},
+   RegionFunction -> Function[{lambdaA, qBA, muA},
+   (1/3*(3*muA + 3*qABVal - 2*qBA) > 0) &&
+   (1/2*(2*muBVal - 3*qABVal + 2*qBA)) > 0 &&
+   qBA < 3*qABVal/2 &&
+   muBVal > 1/2*(3*qABVal - 2*qBA)&&
+   Abs[lambdaA - muA] > tol
+  ],
+  PlotPoints -> 30, MaxRecursion -> 2,
+   Mesh -> None,
+  ContourStyle -> Opacity[1, Yellow]
 ];
 
-combinedPlot = Show[rule1, rule2, AxesLabel -> {"\[Lambda]B", "qAB", "muB"},ViewPoint -> {0, -5, 1}]
+Show[region1,surf1,surf2,AxesLabel -> {"\[Lambda]A", "qBA", "muA"}]
+Show[region2,surf3,surf4,AxesLabel -> {"\[Lambda]A", "qBA", "muA"}]
 
+
+
+(* ::Input:: *)
+(**)
+
+
+(* ::Input:: *)
+(**)
+
+
+(* ::Input:: *)
+(**)
+
+
+(* ::Input:: *)
+(**)
+
+
+(*stationary = [0.5,0.5]*)
+(*Solution surface for lambdaA, qBA and muA*)
+
+(*Fixed values for qAB, muB, and lambdaB*)
+
+(*this scenario satisfies ruleset 1 but not ruleset 2*)
+(*one example will gives us under ruleset 1 qBA = 0.3, muA = 0.2, lambdaA = 0.1 *)
+(*In fact any muA > -qAB + qBA and lambdaA given by the equality will do. For example:
+qBA = 0.3, muA = 0.3, lambdaA= 0.2 *)
+(*In any case, both correspond to the same net diversification in A, so for inferences,
+perhaps it's better to parameterize events using net diversification and turnover (see my slack)*
+
+can design an experiment where we do inference with likelihood function written using actual rates
+and using net diversification and turnover instead*)
+
+(*qABVal = 0.2;
+muBVal = 0.1;
+lambdaBVal = 0.2;
+*)
+(*this scenario satisfies ruleset 2 but not ruleset 1*)
+(*one example will give us under ruleset 2 qBA=0.1, muA = 0.2 and lambdaA = 0.3*)
+(*In fact, any muA > 0 will do, for example: muA = 0.1, which will give us lambda A = 0.2 with qBA = 0.1*)
+
+qABVal = 0.2;
+muBVal = 0.2;
+lambdaBVal = 0.1 ;
+(*tolerance for the lambdaA != muA (and for B)*)
+tol = 0.00001;
+(*Note LambdaB cannot be equal to MuA. Similarly for LambdaA and MuA.*)
+
+(*Ruleset 1*)
+
+(*plot the region bounded by the inequalities *)
+region1 = RegionPlot3D[
+  muA + qABVal - qBA > 0 &&
+  muBVal - qABVal + qBA > 0 &&
+  muA > -qABVal + qBA &&
+   qBA > qABVal &&
+   Abs[lambdaA - muA] > tol &&
+   Abs[lambdaBVal - muBVal] > tol &&
+   lambdaA > 0 &&
+   qBA > 0 &&
+   muA > 0,
+  {lambdaA, 0, 1}, {qBA, 0, 1}, {muA, 0, 1},
+  PlotStyle -> Opacity[0.2, Gray],
+  Mesh -> None
+];
+
+(* plot the plan for lambdaA equality *)
+surf1 = ContourPlot3D[
+  lambdaA - muA - qABVal + qBA == 0,
+  {lambdaA, 0, 1}, {qBA, 0, 1}, {muA, 0, 1},
+  RegionFunction -> Function[{lambdaA, qBA, muA},
+    muA + qABVal - qBA > 0 &&
+    muBVal - qABVal + qBA > 0 &&
+    muA > -qABVal + qBA &&
+    qBA > qABVal &&
+    Abs[lambdaA - muA] > tol &&
+    Abs[lambdaBVal - muBVal] > tol
+    lambdaA > 0 &&
+    qBA > 0 &&
+    muA > 0
+  ],
+  PlotPoints -> 30, MaxRecursion -> 2,
+   Mesh -> None,
+  ContourStyle -> Opacity[1, Red]
+];
+
+(* plot the plan for lambdaB equality *)
+surf2 = ContourPlot3D[
+  lambdaBVal - muBVal + qABVal - qBA == 0,
+   {lambdaA, 0, 1}, {qBA, 0, 1}, {muA, 0, 1},
+  RegionFunction -> Function[{lambdaA, qBA, muA},
+    muA + qABVal - qBA > 0 &&
+    muBVal - qABVal + qBA > 0 &&
+    muA > -qABVal + qBA &&
+    qBA > qABVal &&
+    Abs[lambdaA - muA] > tol &&
+    Abs[lambdaBVal - muBVal] > tol&&
+    lambdaA > 0 &&
+   qBA > 0 &&
+   muA > 0
+  ],
+  PlotPoints -> 30, MaxRecursion -> 2,
+   Mesh -> None,
+  ContourStyle -> Opacity[1, Yellow]
+];
+
+(*Ruleset 2*)
+
+(*plot the region bounded by the inequalities *)
+region2 = RegionPlot3D[
+  muA + qABVal - qBA > 0 &&
+  muBVal - qABVal + qBA > 0 &&
+  qBA < qABVal &&
+  muBVal > qABVal - qBA &&
+   Abs[lambdaA - muA] > tol &&
+   Abs[lambdaBVal - muBVal] > tol &&
+   lambdaA > 0 &&
+   qBA > 0 &&
+   muA > 0,
+  {lambdaA, 0, 1}, {qBA, 0, 1}, {muA, 0, 1},
+  PlotStyle -> Opacity[0.2, Gray],
+  Mesh -> None
+];
+
+(* plot the plan for lambdaA equality *)
+surf3 = ContourPlot3D[
+  lambdaA - muA - qABVal + qBA == 0,
+  {lambdaA, 0, 1}, {qBA, 0, 1}, {muA, 0, 1},
+  RegionFunction -> Function[{lambdaA, qBA, muA},
+  muA + qABVal - qBA > 0 &&
+  muBVal - qABVal + qBA > 0 &&
+  qBA < qABVal &&
+  muBVal > qABVal - qBA &&
+   Abs[lambdaA - muA] > tol &&
+   Abs[lambdaBVal - muBVal] > tol &&
+   lambdaA > 0 &&
+   qBA > 0 &&
+   muA > 0
+  ],
+  PlotPoints -> 30, MaxRecursion -> 2,
+   Mesh -> None,
+  ContourStyle -> Opacity[1, Red]
+];
+
+(* plot the plan for lambdaB equality *)
+surf4 = ContourPlot3D[
+  lambdaBVal - muBVal + qABVal - qBA == 0,
+   {lambdaA, 0, 1}, {qBA, 0, 1}, {muA, 0, 1},
+   RegionFunction -> Function[{lambdaA, qBA, muA},
+  muA + qABVal - qBA > 0 &&
+  muBVal - qABVal + qBA > 0 &&
+  qBA < qABVal &&
+  muBVal > qABVal - qBA &&
+   Abs[lambdaA - muA] > tol &&
+   Abs[lambdaBVal - muBVal] > tol &&
+   lambdaA > 0 &&
+   qBA > 0 &&
+   muA > 0
+  ],
+  PlotPoints -> 30, MaxRecursion -> 2,
+   Mesh -> None,
+  ContourStyle -> Opacity[1, Yellow]
+];
+
+Show[region1,surf1,surf2,AxesLabel -> {"\[Lambda]A", "qBA", "muA"}]
+Show[region2,surf3,surf4,AxesLabel -> {"\[Lambda]A", "qBA", "muA"}]
+
+
+(*stationary = [0.4,0.6]*)
+(*Solution surface for lambdaA, qBA and muA*)
+
+(*Fixed values for qAB, muB, and lambdaB*)
+
+(*this scenario satisfies ruleset 1 but not ruleset 2*)
+(*one example will gives us under ruleset 1 qBA = 0.7/3, muA = 0.2, lambdaA = 0.1/2 *)
+(*In fact, any values where mu_A > 1/2*(-2qAB + 3qBA) and \lambda_A = 1/2(2mu_A+2qAB-3qBA) 
+will lead to the same stationary frequencies.*)
+(*For example: qBA = 0.7/3, muA = 0.4, lambdaA = 0.25*)
+(*Again, the net diversification is A is preserved in these two examples*)
+
+(*qABVal = 0.2;
+muBVal = 0.1;
+lambdaBVal = 0.2;*)
+
+
+(*this scenario satisfies ruleset 2 but not ruleset 1*)
+(*one example will give us under ruleset 2 qBA=0.1/3, muA = 0.1 and lambdaA = 0.25*)
+(*In fact, any values mu_A > 0 and \lambda_A = 1/2(2mu_A+2qAB-3qBA) will do*)
+(*For example: qBA=0.1/3, muA = 0.3 and lambdaA = 0.45 )
+(*Again, the net diversification is A is preserved in these two examples*)
+*)
+
+qABVal = 0.2;
+muBVal = 0.2;
+lambdaBVal = 0.1;
+
+(*the polynomials in scenario 1 and 2 differ but they still lead to the same frequency*)
+
+(*tolerance for the lambdaA != muA (and for B)*)
+tol = 0.00001;
+(*Note LambdaB cannot be equal to MuA. Similarly for LambdaA and MuA.*)
+
+(*Ruleset 1*)
+
+(*plot the region bounded by the inequalities *)
+region1 = RegionPlot3D[
+  1/2*(2*muA + 2*qABVal - 3*qBA) > 0 &&
+  1/3*(3*muBVal - 2*qABVal + 3*qBA) > 0 &&
+  (**)
+   muA > 1/2*(-2*qABVal + 3*qBA) &&
+   qBA > 2*qABVal/3 &&
+   Abs[lambdaA - muA] > tol &&
+   Abs[lambdaBVal - muBVal] > tol &&
+   lambdaA > 0 &&
+   qBA > 0 &&
+   muA > 0,
+  {lambdaA, 0, 1}, {qBA, 0, 1}, {muA, 0, 1},
+  PlotStyle -> Opacity[0.2, Gray],
+  Mesh -> None
+];
+
+(* plot the plan for lambdaA equality *)
+surf1 = ContourPlot3D[
+  lambdaA - 1/2*(2*muA + 2*qABVal - 3*qBA) == 0,
+  {lambdaA, 0, 1}, {qBA, 0, 1}, {muA, 0, 1},
+  RegionFunction -> Function[{lambdaA, qBA, muA},
+    1/2*(2*muA + 2*qABVal - 3*qBA) > 0 &&
+    1/3*(3*muBVal - 2*qABVal + 3*qBA) > 0 &&
+    muA > 1/2*(-2*qABVal + 3*qBA) &&
+    qBA > 2*qABVal/3 &&
+    Abs[lambdaA - muA] > tol &&
+    Abs[lambdaBVal - muBVal] > tol &&
+    lambdaA > 0 &&
+    qBA > 0 &&
+    muA > 0
+  ],
+  PlotPoints -> 30, MaxRecursion -> 2,
+   Mesh -> None,
+  ContourStyle -> Opacity[1, Red]
+];
+
+(* plot the plan for lambdaB equality *)
+surf2 = ContourPlot3D[
+  lambdaBVal - 1/3*(3*muBVal - 2*qABVal + 3*qBA) == 0,
+   {lambdaA, 0, 1}, {qBA, 0, 1}, {muA, 0, 1},
+  RegionFunction -> Function[{lambdaA, qBA, muA},
+    1/2*(2*muA + 2*qABVal - 3*qBA) > 0 &&
+    1/3*(3*muBVal - 2*qABVal + 3*qBA) > 0 &&
+    muA > 1/2*(-2*qABVal + 3*qBA) &&
+    qBA > 2*qABVal/3 &&
+    Abs[lambdaA - muA] > tol &&
+    Abs[lambdaBVal - muBVal] > tol &&
+    lambdaA > 0 &&
+   qBA > 0 &&
+   muA > 0
+  ],
+  PlotPoints -> 30, MaxRecursion -> 2,
+   Mesh -> None,
+  ContourStyle -> Opacity[1, Yellow]
+];
+
+(*Ruleset 2*)
+
+(*plot the region bounded by the inequalities *)
+region2 = RegionPlot3D[
+  1/2*(2*muA + 2*qABVal - 3*qBA) > 0 &&
+  1/3*(3*muBVal - 2*qABVal + 3*qBA) > 0 &&
+  qBA < 2*qABVal/3 &&
+  muBVal > 1/3*(2*qABVal - 3*qBA) &&
+   Abs[lambdaA - muA] > tol &&
+   Abs[lambdaBVal - muBVal] > tol &&
+   lambdaA > 0 &&
+   qBA > 0 &&
+   muA > 0,
+  {lambdaA, 0, 1}, {qBA, 0, 1}, {muA, 0, 1},
+  PlotStyle -> Opacity[0.2, Gray],
+  Mesh -> None
+];
+
+(* plot the plan for lambdaA equality *)
+surf3 = ContourPlot3D[
+  lambdaA - 1/2*(2*muA + 2*qABVal - 3*qBA) == 0,
+  {lambdaA, 0, 1}, {qBA, 0, 1}, {muA, 0, 1},
+  RegionFunction -> Function[{lambdaA, qBA, muA},
+  1/2*(2*muA + 2*qABVal - 3*qBA) > 0 &&
+  1/3*(3*muBVal - 2*qABVal + 3*qBA) > 0 &&
+  qBA < 2*qABVal/3 &&
+  muBVal > 1/3*(2*qABVal - 3*qBA) &&
+   Abs[lambdaA - muA] > tol &&
+   Abs[lambdaBVal - muBVal] > tol &&
+   lambdaA > 0 &&
+   qBA > 0 &&
+   muA > 0
+  ],
+  PlotPoints -> 30, MaxRecursion -> 2,
+   Mesh -> None,
+  ContourStyle -> Opacity[1, Red]
+];
+
+(* plot the plan for lambdaB equality *)
+surf4 = ContourPlot3D[
+  lambdaBVal - 1/3*(3*muBVal - 2*qABVal + 3*qBA) == 0,
+   {lambdaA, 0, 1}, {qBA, 0, 1}, {muA, 0, 1},
+   RegionFunction -> Function[{lambdaA, qBA, muA},
+  1/2*(2*muA + 2*qABVal - 3*qBA) > 0 &&
+  1/3*(3*muBVal - 2*qABVal + 3*qBA) > 0 &&
+  qBA < 2*qABVal/3 &&
+  muBVal > 1/3*(2*qABVal - 3*qBA) &&
+   Abs[lambdaA - muA] > tol &&
+   Abs[lambdaBVal - muBVal] > tol &&
+   lambdaA > 0 &&
+   qBA > 0 &&
+   muA > 0
+  ],
+  PlotPoints -> 30, MaxRecursion -> 2,
+   Mesh -> None,
+  ContourStyle -> Opacity[1, Yellow]
+];
+
+Show[region1,surf1,surf2,AxesLabel -> {"\[Lambda]A", "qBA", "muA"}]
+Show[region2,surf3,surf4,AxesLabel -> {"\[Lambda]A", "qBA", "muA"}]
 
 
 
